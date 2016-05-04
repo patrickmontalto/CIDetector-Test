634328fbe12ab22abcdeb56c371f3b387aba02f8
patrickmontalto <pmontalto@gmail.com>
2016-05-04 15:04:10 -0400

    Initial Commit

---
+++ CIDetectorTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CIDetectorTest
+//
+//  Created by Patrick on 5/4/16.
+//  Copyright © 2016 ADNAS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
